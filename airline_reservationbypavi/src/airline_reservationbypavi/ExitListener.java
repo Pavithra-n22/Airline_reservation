a80349c846ed5e732539d37161e4aff8462e5b38
Pavithra <146511928+Pavithra-n22@users.noreply.github.com>
2024-01-04 21:38:11 +0530

    Add files via upload

---
+++ airline_reservationbypavi/src/airline_reservationbypavi/ExitListener.java
@@ -0,0 +1,12 @@
+package airline_reservationbypavi;
+
+import java.awt.*;
+import java.awt.event.*;
+
+public class ExitListener extends WindowAdapter 
+{
+	public void windowClosing(WindowEvent event) 
+	{
+		System.exit(0);
+	}
+}
